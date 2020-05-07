cce7955b1d83770495f189ca7aa2106246191bd2
Jorge <Jorge@LAPTOP-2U4BDQ1N>
2020-05-07 17:17:18 +0200

    done

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -18,29 +18,42 @@ public class AccessLog {
 
 	// TODO: add an access entry to the log for the provided message and return assigned id
 	public int add(String message) {
-		
-		int id = 0;
-		
+		int id =cid.getAndIncrement();
+		AccessEntry entry = new AccessEntry(id, message);
+		log.put(id, entry);
 		return id;
 	}
 		
 	// TODO: retrieve a specific access entry from the log
 	public AccessEntry get(int id) {
 		
-		return null;
+		return log.get(id);
 		
 	}
 	
 	// TODO: clear the access entry log
 	public void clear() {
-		
+		log.clear();
+		cid.set(0);
 	}
 	
 	// TODO: return JSON representation of the access log
 	public String toJson () {
-    	
-		String json = null;
-    	
-    	return json;
-    }
+		
+		String json = "[\n";
+    	int i = 0;
+		if(log.size()!=0) {
+			while(i<log.size()-1) {
+				json+=("{\n	\"id:\" "+i+",\n 	\"message\": "+ "\" "+log.get(i).getMessage()+"\"\n},\n");
+//				json+= ("{ id: "+i+", message:"+log.get(i).getMessage()+"},");
+				i++;
+			}
+			
+			json+=("{\n	\"id:\" "+i+",\n 	\"message\": "+ "\""+log.get(i).getMessage()+"\"\n}");
+//			json+= ("{ id: "+i+", message:"+log.get(i).getMessage()+"}");
+			
+		}
+		json+="\n]";
+		return json;
+	}
 }
--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -30,6 +30,7 @@ public class App {
 		
 		accesslog = new AccessLog();
 		accesscode  = new AccessCode();
+
 		
 		after((req, res) -> {
   		  res.type("application/json");
@@ -45,6 +46,55 @@ public class App {
 		
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
+		
+		post("/accessdevice/log", (req, res) -> {
+			Gson gson = new Gson();
+			
+			String message=gson.fromJson(req.body(), AccessMessage.class).getMessage();
+			
+			int id = accesslog.add(message);
+			return ("{\n	\"id:\""+id+",\n 	\"message\": "+ "\""+message+"\"\n}");
+		});
+		
+		get("/accessdevice/log/", (req, res) -> {
+		 	return accesslog.toJson();
+		});
+		
+		get("/accessdevice/log/:id", (req, res) -> {
+			String idString=req.params("id");
+			int requestId = Integer.parseInt(idString);
+			AccessEntry a = accesslog.get(requestId);
+			return ("{\n	\"id:\""+a.getId()+",\n 	\"message\": "+ "\""+a.getMessage()+"\"\n}");
+		 	
+		});
+		
+		put("/accessdevice/code", (req, res) -> {
+			
+			Gson gson = new Gson();
+        	
+			int[] code =  gson.fromJson(req.body(), AccessCode.class).getAccesscode();
+        	accesscode.setAccesscode(code);
+        
+            return "{\n	\"accesscode\": [\n	"+code[0]+",\n	"+code[1]+"\n]\n}";
+		});
+		
+		get("/accessdevice/code", (req, res) -> {
+			
+			int[] code = accesscode.getAccesscode();
+		 	String jsonString="{\n	\"accesscode\": [\n	"+code[0]+",\n	"+code[1]+"\n]\n}";
+			return jsonString;
+//		 	Gson gson = new Gson();
+//			return gson.toJson(accesscode);
+		});
+		
+		delete("/accessdevice/log/", (req, res) -> {
+			accesslog.clear();
+			return(accesslog.toJson());
+		});
+		
+		
+		
+		
 		
     }
     
